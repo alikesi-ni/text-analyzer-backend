56148c9b0d4476e8867262cfd119f3102a5ef536
Alexander <alexander.nittmann@gmail.com>
2024-06-28 23:52:18 +0200

    init

---
+++ src/main/java/com/example/textanalyzer/TextAnalyzerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.textanalyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TextAnalyzerBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TextAnalyzerBackendApplication.class, args);
+    }
+
+}
